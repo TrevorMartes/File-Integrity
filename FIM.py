6eb2d257977f892ffcbf1b825f16ff8eec82286d
Trevor <trevor.martes@gmail.com>
2025-07-13 00:30:04 -0700

    Switched baseline to store to a SQLite DB, will work on Integrity checker next.

--- FIM.py
+++ FIM.py
@@ -1,4 +1,4 @@
-import hashlib, time
+import hashlib, time, sqlite3, sys
 
 # Proof of Concept FIM (File Integrity Monitor) 
 print('Welcome to the Simple File Ingrity Monitor!')
@@ -6,15 +6,31 @@ print('Welcome to the Simple File Ingrity Monitor!')
 time.sleep(1)
 
 filepath = input('What file(s) are we monitoring? *please use the filepath ie. ~/home/user/Documents\n\n') #Need the actual file input to be fuctional
-baseline_dic = {'FIM.py': 'd79451ce22f324fe6de47301050ca98d7ad79e3b42c6a16c617a5dcce1aaf625'} #Example hash  *Move to database for security or like the shadow file?
+# baseline_dic = {'FIM.py': 'd79451ce22f324fe6de47301050ca98d7ad79e3b42c6a16c617a5dcce1aaf625'} #Example hash  *Move to database for security or like the shadow file?
+
+# Initializing sqlite db 
+with sqlite3.connect('FIM.db') as connection:
+        cursor = connection.cursor()
+        create_table_query = '''
+        CREATE TABLE IF NOT EXISTS File_Integrity (
+            file TEXT PRIMARY KEY NOT NULL,
+            hash TEXT NOT NULL
+        );
+        '''
+        cursor.execute(create_table_query)
+        connection.commit() 
 
 # Baseline hashing of the file(s) and/or Folder
 def baseline(filepath):
-    # Open the file in read mode ('r') using a 'with' statement
+   # Open the file in read mode ('r') using a 'with' statement
     # The 'with' statement ensures the file is automatically closed, even if errors occur.
-    with open(filepath, 'r') as file:
+    try:
+        with open(filepath, 'r') as file:
         # Read the entire content of the file as a single string
-        content = file.read()
+            content = file.read()
+    except FileNotFoundError:
+        print(f'\nThe file {filepath} does not exist.')
+        sys.exit()
 
     # Encodes the filepath in the utf-8 standard
     encoded_filepath = content.encode('utf-8')
@@ -27,16 +43,19 @@ def baseline(filepath):
     # Get the hexadecimal representation of the hash
     hex_digest = hasher.hexdigest()
 
-    # Add baseline hash to the baseline and if already there, it will check the integrity
-    if filepath not in baseline_dic:
-        baseline_dic.update({filepath:hex_digest})
-        print('\nYour file has been uploaded to the Baseline!')
-        print(f'\nOriginal string: {filepath}\nSHA256 hash: {hex_digest}')
-    elif filepath in baseline_dic:
-        print('\nThis file is already in the baseline! Cheking integrity now...')
-        check_integrity(filepath)
-            
-    #print(baseline_dic) #will remove/ want to have it saved to an encrypted database if possible
+    try:
+            # Attempt to insert, ignoring if a duplicate exists
+            cursor.execute(f'''
+                INSERT OR IGNORE INTO File_Integrity (file, hash) VALUES (?, ?)''',(filepath,hex_digest))
+
+            if cursor.rowcount == 0:
+                print(f"Your file: '{filepath}' already exists in the FIM database.") 
+            else:
+                connection.commit()  # Commit the changes if insertion was successful
+                print(f"Your file: '{filepath}:{hex_digest}' was inserted successfully into the FIM database.")
+
+    except sqlite3.Error as e:
+        print(f"SQLite error: {e}")
 
 #comparing baseline to the current hash
 def check_integrity(filepath):
